15408eaec70fdacebf59ac8d655c6c80cdd01201
chenaey <1343421593cys@gmail.com>
2025-04-09 23:57:58 +0800

    feat: delete unuse file

--- composables/ai/index.ts
+++
@@ -1,6 +0,0 @@
-export { useAICodeGeneration } from './useAICodeGeneration'
-export { useCodeCache } from './useCodeCache'
-export { useLoadingAnimation } from './useLoadingAnimation'
-
-// 导出类型
-export type { GenerationState } from './useAICodeGeneration' 
